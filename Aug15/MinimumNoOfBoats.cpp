c4f529f28c47ca8a254aa3c0ba2aab3b3b80b197
Aditya Thakre <aditcrey@gmail.com>
2018-08-15 15:35:11 +0530

    Create MinimumNoOfBoats.cpp

---
+++ Aug15/MinimumNoOfBoats.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/boats-to-save-people/description/
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        sort(people.begin(),people.end());
+        int ans = 0;
+        int i=0,j=n-1;
+        while(i<j){
+            if(people[i]+people[j]<=limit) {
+                i++;
+            }
+            j--;ans++;
+        }
+        
+        if(i==j) ans++;
+        return ans;
+    }
+};
